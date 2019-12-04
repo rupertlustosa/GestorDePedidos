c8597b5d31f21924cac65b944cd817edb8f81d52
Rupert Lustosa <rupertlustosa@gmail.com>
2019-12-04 02:08:32 -0300

    Adiciona UserServiceProvider

--- config/app.php
+++ config/app.php
@@ -174,6 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Modules\User\Providers\UserServiceProvider::class,
+        //Modules\Order\Providers\OrderServiceProvider::class,
 
     ],
 
